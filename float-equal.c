936d7e6c88389363bf5c471128425ca3ccd52789
Maker-Zheng <55417485+Maker-Zheng@users.noreply.github.com>
2019-10-22 10:28:57 +0800

    Add files via upload

--- float-equal.c
+++ float-equal.c
@@ -6,12 +6,12 @@ int main()
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    double a=z-z1;
-    if (abs(a<=0.00001)) 
+	float a = z>z1 ? z-z1 : z1-z;   
+    if (a<=0.00001) 
 	{
         z = (m + x) / y;
     }
-    printf("Z is %f\n", z);
-    printf("Z1 is %f\n", z1);
+    printf("z is %f\n", z);
+    printf("z1 is %f\n", z1);
     return 0;
 }
